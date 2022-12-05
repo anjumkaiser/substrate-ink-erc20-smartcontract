d882d67f47ae0a7aa059e40914f8a5b7ea3e3945
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2022-12-05 17:32:41 +0500

    erc20 smartcontract code updated to match current substrate tutorials

--- lib.rs
+++ lib.rs
@@ -4,154 +4,54 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod erc20 {
+    use ink_storage::{traits::SpreadAllocate, Mapping};
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    /// Create storage for a simple ERC-20 contract.
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct Erc20 {
-        /// Stores total supply
+        /// Total token supply.
         total_supply: Balance,
-        // The balnce of each user
-        balances: ink_storage::collections::HashMap<AccountId, Balance>,
-        // Approval spender on behalf of the message's sender
-        allowances: ink_storage::collections::HashMap<(AccountId, AccountId), Balance>,
-    }
-
-    #[ink(event)]
-    pub struct Transfer {
-        #[ink(topic)]
-        from: Option<AccountId>,
-        #[ink(topic)]
-        to: Option<AccountId>,
-        #[ink(topic)]
-        value: Balance,
-    }
-
-    #[ink(event)]
-    pub struct Approval {
-        #[ink(topic)]
-        owner: AccountId,
-        #[ink(topic)]
-        spender: AccountId,
-        #[ink(topic)]
-        value: Balance,
+        /// Mapping from owner to number of owned tokens.
+        balances: Mapping<AccountId, Balance>,
     }
 
     impl Erc20 {
-        #[cfg(not(feature = "ink-as-dependency"))]
+        /// Create a new ERC-20 contract with an initial supply.
         #[ink(constructor)]
-        pub fn new(inital_supply: Balance) -> Self {
-            let caller = Self::env().caller();
-            let mut newbalances = ink_storage::collections::HashMap::new();
-            newbalances.insert(caller, inital_supply);
-
-            Self::env().emit_event(Transfer {
-                from: None,
-                to: Some(caller),
-                value: inital_supply,
-            });
-
-            Self {
-                total_supply: inital_supply,
-                balances: newbalances,
-                allowances: ink_storage::collections::HashMap::new(),
-            }
+        pub fn new(initial_supply: Balance) -> Self {
+            // Initialize mapping for the contract.
+            ink_lang::utils::initialize_contract(|contract| {
+                Self::new_init(contract, initial_supply)
+            })
         }
 
+        /// Initialize the ERC-20 contract with the specified initial supply.
+        fn new_init(&mut self, initial_supply: Balance) {
+            let caller = Self::env().caller();
+            self.balances.insert(&caller, &initial_supply);
+            self.total_supply = initial_supply;
+        }
+
+        /// Returns the total token supply.
         #[ink(message)]
         pub fn total_supply(&self) -> Balance {
             self.total_supply
         }
 
+        /// Returns the account balance for the specified `owner`.
         #[ink(message)]
         pub fn balance_of(&self, owner: AccountId) -> Balance {
-            self.balance_of_or_zero(&owner)
-        }
-
-        #[ink(message)]
-        pub fn transfer(&mut self, to: AccountId, value: Balance) -> bool {
-            let caller = self.env().caller();
-            self.transfer_from_to(caller, to, value)
-        }
-
-        #[ink(message)]
-        pub fn approve(&mut self, spender: AccountId, value: Balance) -> bool {
-            let owner = self.env().caller();
-            self.allowances.insert((owner, spender), value);
-            self.env().emit_event(Approval {
-                owner: owner,
-                spender: spender,
-                value: value,
-            });
-
-            true
-        }
-
-        #[ink(message)]
-        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
-            self.allowance_of_or_zero(&owner, &spender)
-        }
-
-        #[ink(message)]
-        pub fn transfer_from(&mut self, from: AccountId, to: AccountId, value: Balance) -> bool {
-            let caller = self.env().caller();
-            let allowance = self.allowance_of_or_zero(&from, &caller);
-            if allowance < value {
-                return false;
-            }
-
-            let transfer_result = self.transfer_from_to(from, to, value);
-            if !transfer_result {
-                return false;
-            }
-
-            self.allowances.insert((from, caller), allowance - value);
-
-            true
-        }
-
-        fn balance_of_or_zero(&self, owner: &AccountId) -> Balance {
-            *self.balances.get(owner).unwrap_or(&0)
-        }
-
-        fn transfer_from_to(&mut self, from: AccountId, to: AccountId, value: Balance) -> bool {
-            let from_balance = self.balance_of_or_zero(&from);
-
-            if from_balance < value {
-                return false;
-            }
-
-            self.balances.insert(from, from_balance - value);
-
-            let to_balance = self.balance_of_or_zero(&to);
-            self.balances.insert(to, to_balance + value);
-
-            self.env().emit_event(Transfer {
-                from: Some(from),
-                to: Some(to),
-                value: value,
-            });
-            true
-        }
-
-        fn allowance_of_or_zero(&self, owner: &AccountId, spender: &AccountId) -> Balance {
-            *self.allowances.get(&(*owner, *spender)).unwrap_or(&0)
+            self.balances.get(owner).unwrap_or_default()
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
         use ink_lang as ink;
 
-        /// We test if the default constructor does its job.
         #[ink::test]
         fn new_works() {
             let contract = Erc20::new(777);
@@ -164,40 +64,6 @@ mod erc20 {
             assert_eq!(contract.total_supply(), 100);
             assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
             assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
-        }
-
-        #[ink::test]
-        fn transfer_works() {
-            let mut contract = Erc20::new(100);
-            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
-            assert!(contract.transfer(AccountId::from([0x0; 32]), 10));
-            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
-            assert!(!contract.transfer(AccountId::from([0x0; 32]), 100));
-        }
-
-        #[ink::test]
-        fn transfer_from_works() {
-            let mut contract = Erc20::new(100);
-            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
-            contract.approve(AccountId::from([0x1; 32]), 20);
-            contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 10);
-            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
-        }
-
-        #[ink::test]
-        fn allowances_works() {
-            let mut contract = Erc20::new(100);
-            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
-            contract.approve(AccountId::from([0x1; 32]), 200);
-            assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 200);
-
-            assert!(contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 50));
-            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 50);
-            assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 150);
-
-            assert!(!contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 100));
-            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 50);
-            assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 150);
         }
     }
 }
